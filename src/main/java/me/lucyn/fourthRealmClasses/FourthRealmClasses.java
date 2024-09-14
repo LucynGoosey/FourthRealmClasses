c756a4ea3886c9ae0406305f2c7d4fd09d7c1173
LucynGoosey <didgeridev@gmail.com>
2024-09-13 20:36:14 -0400

    Project Created

---
+++ src/main/java/me/lucyn/fourthRealmClasses/FourthRealmClasses.java
@@ -0,0 +1,17 @@
+package me.lucyn.fourthRealmClasses;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FourthRealmClasses extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
